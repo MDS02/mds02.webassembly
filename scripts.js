c604937cdca61c25438ccca986a97e66d0a5ec7d
MDS02 <54663783+MDS02@users.noreply.github.com>
2020-06-11 17:22:48 +0200

    Added rekenmachine

--- scripts.js
+++ scripts.js
@@ -10,5 +10,20 @@ function loadWebAssembly(fileName) {
 loadWebAssembly('oppervlakte.wasm')
   .then(instance => {
     oppervlakte = instance.exports._Z11oppervlaktei;
-    console.log('Finished compiling! Ready when you are...');
+    console.log('Finished compiling Oppervlakte! Ready when you are...');
+  });
+
+  let rekenmachine;
+
+function loadWebAssembly(fileName) {
+  return fetch(fileName)
+    .then(response => response.arrayBuffer())
+    .then(buffer => WebAssembly.compile(buffer))
+    .then(module => {return new WebAssembly.Instance(module) });
+};
+  
+loadWebAssembly('rekenmachine.wasm')
+  .then(instance => {
+    rekenmachine = instance.exports.calculate;
+    console.log('Finished compiling Rekenmachine! Ready when you are...');
   });
